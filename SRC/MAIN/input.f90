9696e97f352feef292a177fab3284956584790dc
flavianowilliams <flavianowilliams@gmail.com>
2020-05-22 21:51:05 -0300

    Update input.f90

--- SRC/MAIN/input.f90
+++ SRC/MAIN/input.f90
@@ -1130,9 +1130,12 @@ contains
     case('hrm2')
        m=2
       nprtors=2
-    case('ryck')
-       m=3
-       nprtors=7
+   case('ryck')
+      m=3
+      nprtors=7
+   case('amber')
+      m=4
+      nprtors=4
     end select
 
     return
@@ -1176,6 +1179,9 @@ contains
        nprpar=3
     case('harm')
        m=2
+       nprpar=2
+    case('amber')
+       m=3
        nprpar=2
     end select
 
