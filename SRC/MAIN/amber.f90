ddc71ce74ddf66c37e64dc50f262c2ccb48b19ee
flavianowilliams <flavianowilliams@gmail.com>
2020-05-25 21:43:01 -0300

    Update amber.f90

--- SRC/MAIN/amber.f90
+++ SRC/MAIN/amber.f90
@@ -993,6 +993,14 @@ contains
                    prms(3)=180.0d0
                    prms(4)=2.0d0
                 end select
+             case('OS')
+                select case(pd)
+                case('C ')
+                   prms(1)=1.0d0
+                   prms(2)=0.8d0
+                   prms(3)=180.0d0
+                   prms(4)=1.0d0
+                end select
              end select
           case('S ')
              select case(pc)
