91586f9f0675dc004c5ea4c3046882fd9639b2d5
flavianowilliams <flavianowilliams@gmail.com>
2020-06-02 13:34:39 -0300

    fixing ff

--- SRC/MD/force_field.f90
+++ SRC/MD/force_field.f90
@@ -130,62 +130,63 @@ contains
     k=0
     do i=1,nmolec
        write(6,'(42x,a6,1x,a7)')namemol(i),ff_model(i)
-       write(6,'(20x,111a1)')('*',j=1,52)
-       if(nxmolec(i).le.15)then
-          write(6,'(20x,a6,15(1x,a2))')'Sites:',(atsp(j+k),j=1,nxmolec(i))
+       write(6,'(2x,111a1)')('*',j=1,90)
+       if(nxmolec(i).le.25)then
+          write(6,'(2x,a6,25(1x,a2))')'Sites:',(atsp(j+k),j=1,nxmolec(i))
        else
-          write(6,'(20x,a6,15(1x,a2))')'Sites:',(atsp(j+k),j=1,15)
-          write(6,'(26x,15(1x,a2))')(atsp(j+k),j=16,nxmolec(i))
+          write(6,'(2x,a6,25(1x,a2))')'Sites:',(atsp(j+k),j=1,25)
+          write(6,'(8x,25(1x,a2))')(atsp(j+k),j=26,nxmolec(i))
        end if
        if(ff_model(i).eq.'(AMBER)')then
           write(6,*)
-          write(6,'(20x,a28,2x,f7.4)')'scale factor (eletrostatic):',sf_coul
-          write(6,'(20x,a29,1x,f7.4)')'scale factor (Van der Waals):',sf_vdw
+          write(6,'(2x,a24,2x,f7.4)')'1-4 scale factor (elet):',sf_coul
+          write(6,'(2x,a23,3x,f7.4)')'1-4 scale factor (VdW):',sf_vdw
        end if
        write(6,*)
-       write(6,'(20x,a6,1x,i5)')'Bonds:',bondscnt(i)
-       write(6,'(20x,111a1)')('-',j=1,52)
-       write(6,'(20x,a4,2x,a4,2x,a4,3x,a10)')'Site','Site','Type','Parameters'
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,a6,1x,i5)')'Bonds:',bondscnt(i)
+       write(6,'(2x,111a1)')('-',j=1,52)
+       write(6,'(2x,a4,2x,a4,2x,a4,3x,a10)')'Site','Site','Type','Parameters'
+       write(6,'(2x,111a1)')('-',j=1,52)
        do j=1,bondscnt(i)
           select case(bonds(i,j))
           case(1)
-             write(6,'(20x,2(i3,3x),a4,1x,3f9.2)')(molbond(i,j,l),l=1,2),&
+             write(6,'(2x,2(i3,3x),a4,1x,3f9.2)')(molbond(i,j,l),l=1,2),&
                   'mors',parbnd(i,j,1)*econv,parbnd(i,j,2)*kconv,parbnd(i,j,3)*rconv
           case(2)
-             write(6,'(20x,2(i3,3x),a4,1x,2f9.2)')(molbond(i,j,l),l=1,2),&
+             write(6,'(2x,2(i3,3x),a4,1x,2f9.2)')(molbond(i,j,l),l=1,2),&
                   'harm',parbnd(i,j,1)*(econv/rconv**2.d0),parbnd(i,j,2)*rconv
           case(3)
-             write(6,'(20x,2(i3,3x),a5,2f9.2)')(molbond(i,j,l),l=1,2),&
+             write(6,'(2x,2(i3,3x),a5,2f9.2)')(molbond(i,j,l),l=1,2),&
                   'amber',parbnd(i,j,1)*(econv/rconv**2.d0),parbnd(i,j,2)*rconv
           end select
        end do
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,111a1)')('-',j=1,52)
        write(6,*)
-       write(6,'(20x,a6,1x,i5)')'Bends:',bendscnt(i)
-       write(6,'(20x,111a1)')('-',j=1,52)
-       write(6,'(20x,3(a4,2x),a4,4x,a10)')'Site','Site','Site','Type','Parameters'
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,a6,1x,i5)')'Bends:',bendscnt(i)
+       write(6,'(2x,111a1)')('-',j=1,52)
+       write(6,'(2x,4(a4,1x),a4,4x,a10)')' i ','Site','Site','Site','Type','Parameters'
+       write(6,'(2x,111a1)')('-',j=1,52)
        do j=1,bendscnt(i)
           select case(bends(i,j))
           case(1)
-             write(6,'(20x,3(i3,3x),a4,1x,2f8.1)')(molbend(i,j,l),l=1,3),&
+             write(6,'(2x,4(i3,2x),a4,1x,2f8.1)')j,(molbend(i,j,l),l=1,3),&
                   'harm',parbend(i,j,1)*econv,parbend(i,j,2)*aconv
           case(2)
-             write(6,'(20x,3(i3,3x),a5,2f8.1)')&
+             write(6,'(2x,4(i3,2x),a5,2f8.1)')j,&
                   (molbend(i,j,l),l=1,3),'amber',parbend(i,j,1)*econv,parbend(i,j,2)*aconv
           end select
        end do
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,111a1)')('-',j=1,52)
        write(6,*)
-       write(6,'(20x,a10,1x,i5)')'Dihedrals:',torscnt(i)
-       write(6,'(20x,111a1)')('-',j=1,52)
-       write(6,'(20x,4(a4,2x),a4,4x,a10)')'Site','Site','Site','Site','Type','Parameters'
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,a10,1x,i5)')'Dihedrals:',torscnt(i)
+       write(6,'(2x,111a1)')('-',j=1,90)
+       write(6,'(2x,5(a4,1x),1x,a4,4x,a10)')&
+            ' i ','Site','Site','Site','Site','Type','Parameters'
+       write(6,'(2x,111a1)')('-',j=1,90)
        do j=1,torscnt(i)
           select case(tors(i,j))
           case(1)
-             write(6,'(20x,4(i3,3x),a4,1x,2f8.1)')(moltors(i,j,l),l=1,4),&
+             write(6,'(2x,5(i3,2x),1x,a4,1x,2f8.1)')j,(moltors(i,j,l),l=1,4),&
                   'harm',partors(i,j,1)*econv,partors(i,j,2)*aconv
           case(3)
              f1=partors(i,j,1)*econv
@@ -195,37 +196,50 @@ contains
              f5=partors(i,j,5)*econv
              f6=partors(i,j,6)*econv
              f7=partors(i,j,7)*aconv
-             write(6,'(20x,4(i3,3x),a5,2x,7f8.3)')&
+             write(6,'(2x,5(i3,2x),1x,a4,2x,7f8.1)')j,&
                   (moltors(i,j,l),l=1,4),'ryck',f1,f2,f3,f4,f5,f6,f7
           case(4)
              i1=nint(partors(i,j,1))
              f1=partors(i,j,2)*econv
              f2=partors(i,j,3)*aconv
              i2=nint(partors(i,j,4))
-             write(6,'(20x,4(i3,3x),a5,2x,i2,f8.2,f8.1,i2)')&
+             write(6,'(2x,5(i3,2x),1x,a5,2x,i2,f8.2,f8.1,i2)')j,&
                   (moltors(i,j,l),l=1,4),'amber',i1,f1,f2,i2
           end select
        end do
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,111a1)')('-',j=1,90)
        write(6,*)
-       write(6,'(20x,a19,1x,i5)')'Improper dihedrals:',itorscnt(i)
-       write(6,'(20x,111a1)')('-',j=1,52)
-       write(6,'(20x,4(a4,2x),a4,4x,a10)')'Site','Site','Site','Site','Type','Parameters'
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,a19,1x,i5)')'Improper dihedrals:',itorscnt(i)
+       write(6,'(2x,111a1)')('-',j=1,90)
+       write(6,'(2x,4(a4,2x),a4,4x,a10)')'Site','Site','Site','Site','Type','Parameters'
+       write(6,'(2x,111a1)')('-',j=1,90)
        do j=torscnt(i)+1,(torscnt(i)+itorscnt(i))
           select case(tors(i,j))
+          case(1)
+             write(6,'(2x,5(i3,2x),1x,a4,1x,2f8.1)')j,(moltors(i,j,l),l=1,4),&
+                  'harm',partors(i,j,1)*econv,partors(i,j,2)*aconv
+          case(3)
+             f1=partors(i,j,1)*econv
+             f2=partors(i,j,2)*econv
+             f3=partors(i,j,3)*econv
+             f4=partors(i,j,4)*econv
+             f5=partors(i,j,5)*econv
+             f6=partors(i,j,6)*econv
+             f7=partors(i,j,7)*aconv
+             write(6,'(2x,5(i3,2x),1x,a4,2x,7f8.1)')j,&
+                  (moltors(i,j,l),l=1,4),'ryck',f1,f2,f3,f4,f5,f6,f7
           case(4)
              i1=nint(partors(i,j,1))
              f1=partors(i,j,2)*econv
              f2=partors(i,j,3)*aconv
              i2=nint(partors(i,j,4))
-             write(6,'(20x,4(i3,3x),a5,2x,i2,f8.2,f8.1,i2)')&
+             write(6,'(2x,5(i3,2x),a5,2x,i2,f8.2,f8.1,i2)')j,&
                   (moltors(i,j,l),l=1,4),'amber',i1,f1,f2,i2
           end select
        end do
-       write(6,'(20x,111a1)')('-',j=1,52)
+       write(6,'(2x,111a1)')('-',j=1,90)
        write(6,*)
-       write(6,'(20x,111a1)')('*',j=1,52)
+       write(6,'(2x,111a1)')('*',j=1,90)
        write(6,*)
        write(6,*)
        k=k+nxmolec(i)
