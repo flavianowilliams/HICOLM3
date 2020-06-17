1499aefefe5284a11a5eac26334271d9b57032c7
flavianowilliams <flavianowilliams@gmail.com>
2020-06-17 08:50:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,80 +13,101 @@ path=`pwd`
 exe_dir="/usr/local/bin"
 aux_dir="/usr/local/share"
 #
-# -- uninstalling program
-#
-if [ -f "$exe_dir/hicolm" ]
-then
-    rm $exe_dir/hicolm
-fi
-#
-if [ -f "$exe_dir/hsystem" ]
-then
-    rm $exe_dir/hsystem
-fi
-#
-if [ -f "$exe_dir/hproperties" ]
-then
-    rm $exe_dir/hproperties
-fi
-#
-if [ -f "$exe_dir/hftir" ]
-then
-    rm $exe_dir/hftir
-fi
+# --creation directories
 #
 if [ -d "$aux_dir/HICOLM" ]
 then
     rm -r $aux_dir/HICOLM
 fi
 #
-# --creating directory--
-#
 mkdir $aux_dir/HICOLM
 mkdir $aux_dir/HICOLM/amber
 #
-# --compilling HICOLM--
+# -- installing HICOLM --
+#
+echo
+echo "\e[33mInstalling HICOLM\e[0m"
+echo
+#
+if [ -f "$exe_dir/hicolm" ]
+then
+    rm $exe_dir/hicolm
+fi
 #
 cd $path/src
-#
 make clean
 make
-#
+if [ ! -f "HICOLM" ]
+then
+    echo "\e[31mError in compiling HICOLM. The installation is going to finish!"
+    exit
+fi
 mv $path/src/HICOLM $exe_dir/hicolm
-#
 make clean
 #
-# --compilling system--
+# -- installing hsystem --
+#
+echo
+echo "\e[33mInstalling utilitaries\e[0m"
+echo
+#
+if [ -f "$exe_dir/hsystem" ]
+then
+    rm $exe_dir/hsystem
+fi
 #
 cd $path/contrib/system
-#
 gfortran system.f90 -o hsystem
-#
+if [ ! -f "hsystem" ]
+then
+    echo "\e[31mError in compiling hsystem. The installation is going to finish!"
+    exit
+fi
 mv $path/contrib/system/hsystem $exe_dir/hsystem
 #
-# --compilling properties--
+# -- installing hproperties --
+#
+if [ -f "$exe_dir/hproperties" ]
+then
+    rm $exe_dir/hproperties
+fi
 #
 cd $path/contrib/properties
-#
 make clean
 make
-#
+if [ ! -f "hproperties" ]
+then
+    echo "\e[31mError in compiling hproperties. The installation is going to finish!"
+    exit
+fi
 mv $path/contrib/properties/hproperties $exe_dir/hproperties
-#
 make clean
 #
-# --compilling ftir--
+# -- installing hftir --
+#
+if [ -f "$exe_dir/hftir" ]
+then
+    rm $exe_dir/hftir
+fi
 #
 cd $path/contrib/ftir
-#
 make clean
 make
-#
+if [ ! -f "hftir" ]
+then
+    echo "\e[31mError in compiling hftir. The installation is going to finish!"
+    exit
+fi
 mv $path/contrib/ftir/hftir $exe_dir/hftir
-#
 make clean
 #
 # --copying auxiliary files--
 #
+echo
+echo "\e[33mCopying auxiliary files\e[0m"
+echo
+#
 cp $path/contrib/amber/*.prm $aux_dir/HICOLM/amber/.
 #
+echo "\e[32mThe HICOLM was installed in the $exe_dir directory. To use it, just type hicolm in the terminal.\e[0m"
+echo
