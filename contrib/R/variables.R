2cf9376069ce15830d16f58a51c58c43ef223739
flavianowilliams <flavianowilliams@gmail.com>
2020-08-21 10:44:01 -0300

    Update variables.R

--- contrib/R/variables.R
+++ contrib/R/variables.R
@@ -7,16 +7,11 @@ library(magrittr)
 library(ggplot2)
 
 #setwd("/home/flaviano/Dropbox/Projeto/simulacoes/H2O_MD")
-#setwd("/home/flaviano/Documentos/maria_eduarda")
+setwd("/home/flaviano/Documentos/maria_eduarda")
 
 # carregando dataframe
 
 nx=readr::read_table2(file="HICOLM.md",col_names = FALSE,n_max=1)
-
-dff=data.frame(c("A1","X2","X3","X4","X5","X6","X7"))
-
-dff=readr::read_table2(file="HICOLM.md",col_names = FALSE,skip = 2,skip_empty_rows = TRUE)
-dff$X7=c(seq(1,dim(dff)[1],1))
 
 n1=as.integer(nx[1,"X1"])
 n2=as.numeric(nx[1,"X2"])
@@ -28,14 +23,21 @@ nxx=c(n1,n2,n3,n4,n5)
 
 dfn=as.integer(3*nxx[4]+7)
 
-dt=dff %>% filter(mod(dff$X7,dfn)==2) %>% select("X1") %>% rename(TIME=X1)
-dv=dff %>% filter(mod(dff$X7,dfn)==2) %>% select("X2") %>% rename(VOLUME=X2)
-dtemp=dff %>% filter(mod(dff$X7,dfn)==2) %>% select("X3") %>% rename(TEMPERATURE=X3)
-dpress=dff %>% filter(mod(dff$X7,dfn)==2) %>% select("X4") %>% rename(PRESSURE=X4)
-de=dff %>% filter(mod(dff$X7,dfn)==2) %>% select("X6") %>% rename(ENERGY=X6)
-drho=dff %>% filter(mod(dff$X7,dfn)==3) %>% select("X1") %>% rename(DENSITY=X1)
+dff=readr::read_table2(file="HICOLM.md",col_names = FALSE,skip = 2,skip_empty_rows = TRUE)
+
+df1=dff[seq(2,nrow(dff),dfn),]
+df2=dff[seq(3,nrow(dff),dfn),]
+
+dt=df1 %>% select("X1")
+dv=df1 %>% select("X2")
+dtemp=df1 %>% select("X3")
+dpress=df1 %>% select("X4")
+de=df1 %>% select("X6")
+drho=df2 %>% select("X1") %>% rename()
 
 df=data.frame(dt,dv,dtemp,dpress,de,drho)
+
+head(df)
 
 #remove(nxx,nx,n1,n2,n3,n4,n5,dt,dv,dtemp,dpress,de,drho,dff,dfn)
 
