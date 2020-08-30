c00bb9e9f15da2ee6f94aec4b54977dfe8594e54
flavianowilliams <flavianowilliams@gmail.com>
2020-08-30 19:21:44 -0300

    Update variables.R

--- contrib/R/variables.R
+++ contrib/R/variables.R
@@ -2,11 +2,11 @@
 
 dff=readr::read_table2(file="HICOLM.md",col_names =
                          c("V1","V2","V3","V4","V5","V6"),
-                       skip_empty_rows = F)
+                       skip_empty_rows = TRUE)
 
 nxx=as.integer(dff[1,4])
 
-dfn=as.integer(3*nxx+9)
+dfn=as.integer(3*nxx+7)
 
 df=dff[seq(4,nrow(dff),dfn),]
 df[,7]=dff[seq(5,nrow(dff),dfn),1]
